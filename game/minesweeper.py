038724f4c0698e265061aa86bb5d473032a417d8
Juan Lanuza <juan.lanuza@invgate.com>
2021-01-25 23:19:46 -0300

    Each cell has a state and knows its adjacent cells

--- game/minesweeper.py
+++ game/minesweeper.py
@@ -1,4 +1,6 @@
+import itertools
 import random
+from enum import Enum
 
 
 class MinesweeperException(Exception):
@@ -6,17 +8,59 @@ class MinesweeperException(Exception):
 
 
 class MinesweeperGame:
-    def __init__(self, columns: int, rows: int, mines: int):
+    def __init__(self, columns: int, rows: int, mines: int, board=None):
         if mines > rows * columns:
             raise MinesweeperException("The board can not have more mines than cells")
 
         self.rows = rows
         self.columns = columns
         self.mines = mines
-        self.board = self._create_board()
+        self.was_lost = False
+        self.was_won = False
+        if board:
+            self.board = board
+        else:
+            self.board = self._create_board()
+        self._set_adjacent_cells()
+
+    @property
+    def is_over(self):
+        return self.was_won or self.was_lost
+
+    @classmethod
+    def from_board(cls, board):
+        columns = len(board)
+        rows = len(board[0])
+        mines = cls.get_mine_count(board)
+        return cls(columns, rows, mines, board)
+
+    @staticmethod
+    def get_mine_count(board):
+        columns = len(board)
+        rows = len(board[0])
+        mines = 0
+        for x_position in range(columns):
+            for y_position in range(rows):
+                if board[x_position][y_position].has_mine:
+                    mines += 1
+        return mines
 
     def get_cell(self, x_position: int, y_position: int):
         return self.board[x_position][y_position]
+
+    @property
+    def visible_board(self):
+        return [
+            [self.get_cell(x_position, y_position).visible_state for y_position in range(self.rows)]
+            for x_position in range(self.columns)
+        ]
+
+    def reveal_cell(self, x_position: int, y_position: int):
+        cell = self.get_cell(x_position, y_position)
+        visible_state = cell.reveal()
+        if visible_state == VisibleCellState.MINE:
+            self.was_lost = True
+        return visible_state
 
     def _create_board(self):
         """
@@ -29,10 +73,30 @@ class MinesweeperGame:
             [MinesweeperCell(x_position, y_position) for y_position in range(self.rows)]
             for x_position in range(self.columns)
         ]
-        mine_positions = [(random.randrange(0, self.columns), random.randrange(0, self.rows)) for i in range(self.mines)]
+        mine_positions = random.sample(list(itertools.product(range(self.columns), range(self.rows))), k=self.mines)
+
         for x_position, y_position in mine_positions:
             board[x_position][y_position].add_mine()
         return board
+
+    def _set_adjacent_cells(self):
+        for x_position in range(self.columns):
+            for y_position in range(self.rows):
+                cell = self.get_cell(x_position, y_position)
+                cell.set_adjacent_cells(self.get_adjacent_cells(cell))
+
+    def get_adjacent_cells(self, cell):
+        x_position = cell.x_position
+        y_position = cell.y_position
+        return {self.get_cell(x, y)
+                for x in range(max(x_position-1, 0), min(x_position+2, self.columns))
+                for y in range(max(y_position-1, 0), min(y_position+2, self.rows))
+                if (x, y) != (x_position, y_position)}
+
+
+class VisibleCellState(Enum):
+    HIDDEN = 0
+    MINE = 1
 
 
 class MinesweeperCell:
@@ -40,7 +104,24 @@ class MinesweeperCell:
         self.x_position = x_position
         self.y_position = y_position
         self.has_mine = False
+        self.is_revealed = False
+        self.adjacent_cells = []
 
     def add_mine(self):
         self.has_mine = True
 
+    def set_adjacent_cells(self, cells):
+        self.adjacent_cells = cells
+
+    def reveal(self):
+        self.is_revealed = True
+        return self.visible_state
+
+    @property
+    def visible_state(self):
+        """
+        State of the cell that the player sees
+        """
+        if self.is_revealed and self.has_mine:
+            return VisibleCellState.MINE
+        return VisibleCellState.HIDDEN
--- game/tests/test_game.py
+++ game/tests/test_game.py
@@ -1,6 +1,6 @@
 import pytest
 
-from game.minesweeper import MinesweeperGame, MinesweeperException
+from game.minesweeper import MinesweeperGame, MinesweeperException, VisibleCellState, MinesweeperCell
 
 
 def test_cant_create_game_with_more_mines_than_cells():
@@ -9,18 +9,89 @@ def test_cant_create_game_with_more_mines_than_cells():
     assert str(excinfo.value) == "The board can not have more mines than cells"
 
 
-def test_create_game_makes_a_board_with_correct_dimensions_and_mine_count():
+def test_create_game_makes_a_board_with_correct_dimensions_mine_count_and_all_cells_hidden():
     columns = 4
     rows = 10
     expected_mine_count = 5
     game = MinesweeperGame(columns, rows, expected_mine_count)
     assert len(game.board) == 4
     assert all([len(column) == 10 for column in game.board])
+    assert not game.is_over
 
     actual_mine_count = 0
     for x_position in range(columns):
         for y_position in range(rows):
-            if game.get_cell(x_position, y_position).has_mine:
+            cell = game.get_cell(x_position, y_position)
+            assert cell.visible_state == VisibleCellState.HIDDEN
+            if cell.has_mine:
                 actual_mine_count += 1
 
     assert actual_mine_count == expected_mine_count
+
+
+def test_player_starts_with_all_cells_hidden():
+    board = [
+        [MinesweeperCell(x_position, y_position) for y_position in range(5)] for x_position in range(5)
+    ]
+
+    game = MinesweeperGame.from_board(board)
+    HIDDEN = VisibleCellState.HIDDEN
+    expected_board = [[HIDDEN, HIDDEN, HIDDEN, HIDDEN, HIDDEN],
+                      [HIDDEN, HIDDEN, HIDDEN, HIDDEN, HIDDEN],
+                      [HIDDEN, HIDDEN, HIDDEN, HIDDEN, HIDDEN],
+                      [HIDDEN, HIDDEN, HIDDEN, HIDDEN, HIDDEN],
+                      [HIDDEN, HIDDEN, HIDDEN, HIDDEN, HIDDEN]]
+    assert game.visible_board == expected_board
+
+
+def test_revealing_a_cell_with_a_mine_ends_the_game_as_looser():
+    board = [
+        [MinesweeperCell(x_position, y_position) for y_position in range(5)] for x_position in range(5)
+    ]
+    board[0][0].add_mine()
+
+    game = MinesweeperGame.from_board(board)
+    cell_state = game.reveal_cell(0, 0)
+    assert cell_state == VisibleCellState.MINE
+    assert game.is_over
+
+
+def test_adjacent_cells_of_each_cell_can_be_accessed():
+    board = [
+        [MinesweeperCell(x_position, y_position) for y_position in range(4)] for x_position in range(4)
+    ]
+
+    game = MinesweeperGame.from_board(board)
+
+    cell = game.get_cell(0, 0)
+    adjacent_cells = {(adj_cell.x_position, adj_cell.y_position) for adj_cell in cell.adjacent_cells}
+    assert adjacent_cells == {        (1, 0),
+                              (0, 1), (1, 1)}
+
+    cell = game.get_cell(0, 1)
+    adjacent_cells = {(adj_cell.x_position, adj_cell.y_position) for adj_cell in cell.adjacent_cells}
+    assert adjacent_cells == {(0, 0), (1, 0),
+                                      (1, 1),
+                              (0, 2), (1, 2)}
+
+    cell = game.get_cell(1, 1)
+    adjacent_cells = {(adj_cell.x_position, adj_cell.y_position) for adj_cell in cell.adjacent_cells}
+    assert adjacent_cells == {(0, 0), (1, 0), (2, 0),
+                              (0, 1),         (2, 1),
+                              (0, 2), (1, 2), (2, 2)}
+
+    cell = game.get_cell(3, 3)
+    adjacent_cells = {(adj_cell.x_position, adj_cell.y_position) for adj_cell in cell.adjacent_cells}
+    assert adjacent_cells == {(2, 2), (3, 2),
+                              (2, 3)         }
+
+    cell = game.get_cell(3, 0)
+    adjacent_cells = {(adj_cell.x_position, adj_cell.y_position) for adj_cell in cell.adjacent_cells}
+    assert adjacent_cells == {(2, 0),
+                              (2, 1), (3, 1)}
+
+    cell = game.get_cell(1, 3)
+    adjacent_cells = {(adj_cell.x_position, adj_cell.y_position) for adj_cell in cell.adjacent_cells}
+    assert adjacent_cells == {(0, 2), (1, 2), (2, 2),
+                              (0, 3),         (2, 3)}
+
