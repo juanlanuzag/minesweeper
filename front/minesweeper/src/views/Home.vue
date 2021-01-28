9517e93e8613c1367e33019ed08af426e5900248
Juan Lanuza <juan.lanuza@invgate.com>
2021-01-28 02:13:14 -0300

    Vue initial files

---
+++ front/minesweeper/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    {{info}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:8000/api/minesweeper/')
+      .then(response => (this.info = response))
+  }
+}
+</script>
